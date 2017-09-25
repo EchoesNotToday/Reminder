bc364a445e255cb9a909c2ffa3f4951a598b7985
Eliez <eliez_l@hotmail.com>
2017-09-25 11:23:59 +0200

    Fixed Cascade delete User

--- database/migrations/2017_09_22_071524_post_it.php
+++ database/migrations/2017_09_22_071524_post_it.php
@@ -16,7 +16,7 @@ class PostIt extends Migration
         Schema::create('post_it', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('titre');
             $table->string('contenu');
             $table->string('couleur');
