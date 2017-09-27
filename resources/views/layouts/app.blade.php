bf801508d11908527eaf7dd869454c7c731504ad
Eliez <eliez_l@hotmail.com>
2017-09-27 16:36:14 +0200

    Fixed modal in app.blade
    Configuration of app to nest all the blade correctly. The modal now send postit to the db.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,14 +23,15 @@
             <div class="modal-dialog">
 
                 <!-- Modal content-->
-                <div class="modal-content">
+                <form class="modal-content" method="post" action="{{ route('home') }}" id="form-casse-couille">
+                    {{ csrf_field() }}
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                         <h4 class="modal-title">Nouveau Post-it</h4>
                     </div>
                     <div class="modal-body">
-                        <input type="text" class="form-control titre" id="text" placeholder="Titre">
-                        <textarea class="contenu form-control" placeholder="Contenu" rows="5" id="comment"></textarea>
+                        <input type="text" class="form-control titre" id="text" name="title" placeholder="Titre">
+                        <textarea class="contenu form-control" name="contenu" placeholder="Contenu" rows="5" id="comment"></textarea>
 
                         <div class="btn-group">
                             <button type="button" class="vert btn btn-primary">Peu important</button>
@@ -41,97 +42,98 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Valider</button>
+                        <button type="submit" class="btn btn-default" data-dismiss="modal" onclick="event.preventDefault();
+                        document.getElementById('form-casse-couille').submit();">Valider</button>
                     </div>
-                </div>
+                </form>
 
             </div>
 
         </div>
     </div>
     <header>
-    @guest
-    <nav class="navbar">
-                    <a class="navbar-brand" href="{{ url('/') }}"><img src="{{ asset('img/logotype.svg') }}"></a>
-                    <ul class="nav navbar-right">
-                        <li class="connect">
-                            <a href="{{ route('login') }}">
-                                <p>Connexion</p><img class="icon" src="{{ asset ('img/login-square-arrow-button-outline.svg')}}" alt="#"></a>
-                        </li>
-                        <li class="signin">
-                            <a href="{{ route('register') }}">
-                                <p>Inscription</p><img class="icon" src="{{ asset ('img/new-file.svg')}}" alt="#"></a>
+        @guest
+        <nav class="navbar">
+            <a class="navbar-brand" href="{{ url('/') }}"><img src="{{ asset('img/logotype.svg') }}"></a>
+            <ul class="nav navbar-right">
+                <li class="connect">
+                    <a href="{{ route('login') }}">
+                        <p>Connexion</p><img class="icon" src="{{ asset ('img/login-square-arrow-button-outline.svg')}}" alt="#"></a>
+                    </li>
+                    <li class="signin">
+                        <a href="{{ route('register') }}">
+                            <p>Inscription</p><img class="icon" src="{{ asset ('img/new-file.svg')}}" alt="#"></a>
                         </li>
                     </ul>
                 </nav>
-        @else
-        <nav class="navbar">
-            <a class="navbar-brand" href="{{ url('/') }}"><img src="{{asset('img/logotype.svg')}}"></a>
+                @else
+                <nav class="navbar">
+                    <a class="navbar-brand" href="{{ url('/') }}"><img src="{{asset('img/logotype.svg')}}"></a>
 
-            <form class="navbar-form navbar-left">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Rechercher">
-                </div>
-                <button type="submit" class="btn btn-default"><img class="searchIcon" src="{{ asset('img/magnifying-glass.svg')}}" alt="#"></button>
-            </form>
+                    <form class="navbar-form navbar-left">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Rechercher">
+                        </div>
+                        <button type="submit" class="btn btn-default"><img class="searchIcon" src="{{ asset('img/magnifying-glass.svg')}}" alt="#"></button>
+                    </form>
 
-            <ul class="nav navbar-right">
+                    <ul class="nav navbar-right">
 
-                <li class="connect">
-                    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">
-                        <img src="{{ asset('img/square-add-button.svg')}}" alt="#">
-                        <p>Ajouter un post-it</p>
-                    </button>
-                </li>
-                <li class="account">
-                    <a href="#" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">
-                        <img class="avatar" src="{{asset('img/chirac.jpg')}}" alt="#">
-                        <p>{{ Auth::user()->name }}</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();"><img class="disconnect" src="{{asset('img/logout.svg')}}" alt="#"></a>
+                        <li class="connect">
+                            <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">
+                                <img src="{{ asset('img/square-add-button.svg')}}" alt="#">
+                                <p>Ajouter un post-it</p>
+                            </button>
+                        </li>
+                        <li class="account">
+                            <a href="#" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">
+                                <img class="avatar" src="{{asset('img/chirac.jpg')}}" alt="#">
+                                <p>{{ Auth::user()->name }}</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"><img class="disconnect" src="{{asset('img/logout.svg')}}" alt="#"></a>
 
 
-                                       <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}>
-                        
-                </li>
-                <li>
-                    <a href="{{ url('/currentUserDelete')}}">Delete User</a>
-                </li>
-                </ul>
-            </nav>
-            @endguest
-        </header>
-        @yield('content')
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}>
 
-        <footer class="footer container-fluid">
-            <ul>
-                <li>
-                    <a href="#"><img class="socialIcon" src="{{ asset('img/facebook-logo.svg')}}" alt="#">
-                    </a>
-                </li>
-                <li>
-                    <a href="#"><img class="socialIcon" src="{{ asset('img/twitter-logo.svg')}}" alt="#">
-                    </a>
-                </li>
-                <li>
-                    <a href="#"><img class="socialIcon" src="{{asset('img/linkedin-sign.svg')}}" alt="#">
-                    </a>
-                </li>
-            </ul>
+                            </li>
+                            <li>
+                                <a href="{{ url('/currentUserDelete')}}">Delete User</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    @endguest
+                </header>
+                @yield('content')
 
-            <p class="mentions">Mentions légales</p>
+                <footer class="footer container-fluid">
+                    <ul>
+                        <li>
+                            <a href="#"><img class="socialIcon" src="{{ asset('img/facebook-logo.svg')}}" alt="#">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#"><img class="socialIcon" src="{{ asset('img/twitter-logo.svg')}}" alt="#">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#"><img class="socialIcon" src="{{asset('img/linkedin-sign.svg')}}" alt="#">
+                            </a>
+                        </li>
+                    </ul>
 
-        </footer>
-    </div>
-    <!-- Scripts -->
-    <script src="{{ asset('js/jquery-3.2.1.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.js') }}"></script>
-    <script src="{{ asset('js/script.js') }}"></script>
-</body>
+                    <p class="mentions">Mentions légales</p>
 
-</html>
+                </footer>
+            </div>
+            <!-- Scripts -->
+            <script src="{{ asset('js/jquery-3.2.1.js') }}"></script>
+            <script src="{{ asset('js/bootstrap.js') }}"></script>
+            <script src="{{ asset('js/script.js') }}"></script>
+        </body>
+
+        </html>
