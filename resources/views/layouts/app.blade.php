e49436d608ec6eb3bc8ca12eb46a63192fead5d8
Eliez <eliez_l@hotmail.com>
2017-09-27 17:29:43 +0200

    Fixed title home page
    use to be Laravel changed to Reminder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Reminder') }}</title>
+    <title>Reminder</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
