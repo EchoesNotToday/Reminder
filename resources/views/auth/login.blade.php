8a0ad2f78b5ebb74b1177c80c9447bf9f491e6b0
Eliez <eliez_l@hotmail.com>
2017-09-28 10:18:37 +0200

    Add to login.blade css & html structure

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app') @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -57,5 +57,43 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+<!-- FORMULAIRE CONNEXION -->
+<div class="blockTriangles">
+            <img class="triangle1" src="{{asset('img/triangles.svg')}}" alt="#">
+            <img class="triangle2" src="{{asset('img/triangles.svg')}}" alt="#">
+        </div>
+        <img class="logo" src="{{asset('img/logoshadow.svg')}}" alt="#">
+        <section id="contenuAccueil">
+            <div class="formulaire">
+                <p>Connexion</p>
+                <form class="form-horizontal" method="POST" action="{{ route('login') }}">{{ csrf_field() }}
+                    <div class="form-group">
+                        <div class="col-sm-12">
+                            <input for="name" type="text" class="form-control" id="name" placeholder="Nom / E-mail" name="name"/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-12">
+                            <input for="password" name="password" type="password" class="form-control" id="password" placeholder="Mot de passe"/>
+                        </div>
+                    </div>
+                    <a href="{{ route('password.request') }}">
+                        <p class="oubli">mot de passe oublié ?</p>
+                    </a>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-12">
+                            <button type="submit" class="btn btn-default">Valider</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            
+            <!-- FIN FORMULAIRE -->
+            <div class="image">
+                <img class="postits" src="{{asset('img/postit.svg')}}" alt="#">
+            </div>
+
+
+        </section>
 @endsection
